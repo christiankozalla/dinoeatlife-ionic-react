45d50780401a77c3ade40984f86019814871a644
christiankozalla <christian.kozalla@gmail.com>
2021-08-11 20:27:49 +0200

    Adds global CSS and page__header on home page

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,9 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Global styles */
+import './theme/global.css';
+
 /* Global State Provider */
 import { AppContextProvider } from './State';
 
@@ -55,6 +58,7 @@ const App: React.FC = () => (
               <Redirect to='/home' />
             </Route>
           </IonRouterOutlet>
+
           <IonTabBar slot='bottom'>
             <IonTabButton tab='home' href='/home'>
               <IonIcon icon={homeOutline} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { FRONTEND_BASE_URL } from '../constants';
+import Login from '../components/Login';
+
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -15,6 +18,22 @@ const Home: React.FC = () => {
             <IonTitle size='large'>DinoEat Home</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <main className='page'>
+          <div className='page__header'>
+            <div className='page__header__text'>
+              <h1 className='page__header__title'>Welcome to DinoEat Life!</h1>
+              <h2 className='page__header__subtitle'>
+                Spice up your life, enhance your cooking and dining experience with your family!
+              </h2>
+            </div>
+            <IonImg
+              src={`${FRONTEND_BASE_URL}/assets/dino-red.png`}
+              className='page__header__image'
+            />
+          </div>
+
+          <Login />
+        </main>
       </IonContent>
     </IonPage>
   );
